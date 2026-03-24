70764f044a96f061217c761f93879c41d247007d
Chloe McFarlane <cmcfar@bu.edu>
2026-03-24 10:09:19 -0400

    Create new app, edit old app, and rename old app to detail March 24, 2026

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,18 +1,15 @@
-import { StyleSheet, ScrollView, View} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import { ScrollView } from 'react-native/types_generated/index';
 
-export default function TabOneScreen() {
+export default function TabTwoScreen() {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-      <Text style={styles.title}>Index</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>About</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-      </View>
-    </ScrollView>
+      <EditScreenInfo path="app/(tabs)/about.tsx" />
+    </View>
   );
 }
 
