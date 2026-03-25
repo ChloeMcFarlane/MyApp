2d28468155f58f09130c651b3c228e79e5ba41d4
Chloe McFarlane <cmcfar@bu.edu>
2026-03-25 09:31:55 -0400

    Continue work on image issues - March 25, 2026

--- app/(tabs)/detail.tsx
+++ app/(tabs)/detail.tsx
@@ -9,26 +9,25 @@ export default function DetailScreen() {
     <ScrollView>
       <View style={styles.container}>
         <Text style={styles.title}>Detail</Text>
+        {/* INTEREST 1 */}
+        <View style={styles.container}>
+          {/* <Image source={{ uri: 'https://cs-people.bu.edu/cmcfar/images'}} style={styles.img} /> */}
+          <Image source={{ uri: '#'}} style={styles.img} />
+          <Text style={styles.info}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa labore aliquid, mollitia tempora praesentium, placeat, assumenda at hic sapiente dicta corporis quisquam. Totam saepe a fuga? Nam, deserunt fuga.</Text>
+        </View>
+        {/* INTEREST 2 */}
+        <View style={styles.container}>
+          <Image source={{ uri: '#'}} style={styles.img} />
+          <Text style={styles.info}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa labore aliquid, mollitia tempora praesentium, placeat, assumenda at hic sapiente dicta corporis quisquam. Totam saepe a fuga? Nam, deserunt fuga.</Text>
+        </View>
+        {/* INTEREST 3 */}
+        <View style={styles.container}>
+          <Image source={{ uri: '#'}} style={styles.img} />
+          <Text style={styles.info}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa labore aliquid, mollitia tempora praesentium, placeat, assumenda at hic sapiente dicta corporis quisquam. Totam saepe a fuga? Nam, deserunt fuga.</Text>
+        </View>
         {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
     </View>
     </ScrollView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//   },
-//   separator: {
-//     marginVertical: 30,
-//     height: 1,
-//     width: '80%',
-//   },
-// });
